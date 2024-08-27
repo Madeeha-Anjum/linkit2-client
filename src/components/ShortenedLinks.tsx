3eae98b616f4758f9c3902abde8795de128272cf
PranavB6 <33413226+PranavB6@users.noreply.github.com>
2024-08-26 23:00:26 -0600

    feat: :sparkles: Various improvements to shortened link cards (#11)

--- src/components/ShortenedLinks.tsx
+++ src/components/ShortenedLinks.tsx
@@ -2,35 +2,21 @@
 
 import { useContext } from 'react';
 
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import LinkItContext from '@/stores/linkit-context';
 
-import { Input } from './ui/input';
+import ShortenedLinkCard from './ShortenedLinkCard';
 
 export default function ShortenedLink() {
   const linkItContext = useContext(LinkItContext);
 
-  if (linkItContext === null || linkItContext.linkRecords.length === 0) {
+  if (linkItContext === null) {
     return null;
   }
 
-  const linkRecords = linkItContext.linkRecords;
-  const fullShortenedUrl = (slug: string) =>
-    window.location.protocol + '//' + window.location.host + '/' + slug;
-
   return (
     <div className="grid grid-cols-1 gap-6">
-      {linkRecords.map((linkRecord) => (
-        <Card key={linkRecord.id}>
-          <CardHeader>
-            <CardTitle>{fullShortenedUrl(linkRecord.slug)}</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <pre className="rounded-md p-4 text-sm overflow-x-auto">
-              <code className=" ">{JSON.stringify(linkRecord, null, 2)}</code>
-            </pre>
-          </CardContent>
-        </Card>
+      {linkItContext.linkIds.map((linkId) => (
+        <ShortenedLinkCard key={linkId} linkId={linkId} />
       ))}
     </div>
   );
